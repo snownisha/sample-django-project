532126d1139f52cb6354406b7b98f8f7b7791ea0
snownisha <snownisha123@gmail.com>
2024-02-28 07:25:59 +0100

    after adding hello/name

--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+import re
+from django.utils.timezone import datetime
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
     return HttpResponse("Hello, Django!")
+
+def hello_there(request, name):
+    print(request.build_absolute_uri()) #optional
+    return render(
+        request,
+        'hello/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
--- web_project/urls.py
+++ web_project/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("", include("hello.urls")),
@@ -22,3 +23,4 @@ urlpatterns = [
 ]
 
 
+urlpatterns += staticfiles_urlpatterns()
